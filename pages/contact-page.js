2f7dac688395b9e46b9575c32bd846eabbeac931
TatsunoriOchi <t.ochi.6482@gmail.com>
2024-06-14 16:26:52 +0900

    change to function

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import Image from "next/image";
 
-const Contact = () => {
+export default function Contact() {
   return (
     <Layout title="Contact">
       <div className="bg-white text-center p-8 w-80 shadow-xl">
@@ -73,5 +73,3 @@ const Contact = () => {
     </Layout>
   );
 };
-
-export default Contact;
